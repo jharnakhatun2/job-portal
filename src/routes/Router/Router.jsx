eb05527105ca55fab2cc754ced56c3723bb96206
Jharna203 <jharnakhatun203@gmail.com>
2025-03-16 09:44:26 +0600

    Created Route, Home, Layout pages

--- src/routes/Router/Router.jsx
+++ src/routes/Router/Router.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "../../layout/Main/MainLayout";
+import Home from "../../pages/Home/Home";
 
 const Router = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   )
 };
 
