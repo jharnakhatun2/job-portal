ab6325d44eebaf6ac17d97cb98780fd7bca02636
Jharna203 <jharnakhatun203@gmail.com>
2025-03-20 08:44:35 +0600

    Created context file and wrap it in main.jsx

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from "react"
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const authInfo = {}
+
+    return (
+        <AuthContext.Provider value={authInfo} >
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export default AuthProvider;
+export const authApp = () => useContext(AuthContext);
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './routes/Router/Router.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <Router/>
+    </AuthProvider>
   </StrictMode>,
 )
