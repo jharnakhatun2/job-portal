5e90964ec0007b4bbcd74bac8554faca32118761
Jharna203 <jharnakhatun203@gmail.com>
2025-03-21 11:49:27 +0600

    Create bottom footer

--- src/layout/Main/MainLayout.jsx
+++ src/layout/Main/MainLayout.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { Outlet } from "react-router";
 import Navbar from "../../pages/Share/Navebar";
+import Footer from "../../pages/Share/Footer";
 
 
 const MainLayout = () => {
   return (
     <div>
-        <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   )
 };
