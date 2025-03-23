45886993f472de80ecadcf7201ef8626af24b5f6
Jharna203 <jharnakhatun203@gmail.com>
2025-03-23 22:35:31 +0600

    Added footer bg and content color

--- src/pages/Share/Footer.jsx
+++ src/pages/Share/Footer.jsx
@@ -4,10 +4,10 @@ import BottomFooter from "./BottomFooter";
 const Footer = () => {
     return (
         <>
-            <footer className="bg-base-200 text-base-content p-10 ">
+            <footer className="bg-zinc-800 text-gray-500 p-10 ">
                 <div className="footer sm:footer-horizontal max-w-7xl mx-auto">
                     <form>
-                        <h6 className="footer-title">Newsletter</h6>
+                        <h6 className="footer-title text-white">Newsletter</h6>
                         <fieldset className="w-80">
                             <label>Enter your email address</label>
                             <div className="join">
@@ -20,20 +20,20 @@ const Footer = () => {
                         </fieldset>
                     </form>
                     <nav>
-                        <h6 className="footer-title">Company</h6>
+                        <h6 className="footer-title text-white">Company</h6>
                         <a className="link link-hover">About us</a>
                         <a className="link link-hover">Contact</a>
                         <a className="link link-hover">Jobs</a>
                         <a className="link link-hover">Press kit</a>
                     </nav>
                     <nav>
-                        <h6 className="footer-title">Legal</h6>
+                        <h6 className="footer-title text-white">Legal</h6>
                         <a className="link link-hover">Terms of use</a>
                         <a className="link link-hover">Privacy policy</a>
                         <a className="link link-hover">Cookie policy</a>
                     </nav>
                     <nav>
-                        <h6 className="footer-title">Social</h6>
+                        <h6 className="footer-title text-white">Social</h6>
                         <div className="grid grid-flow-col gap-4">
                             <a>
                                 <svg
