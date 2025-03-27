686d80f2507a2227fe2dd40a49a58d6bf37bdc2f
Jharna203 <jharnakhatun203@gmail.com>
2025-03-27 23:13:53 +0600

    Added style in footer content

--- src/pages/Share/BottomFooter.jsx
+++ src/pages/Share/BottomFooter.jsx
@@ -9,10 +9,10 @@ const BottomFooter = () => {
 
                 <aside className="grid-flow-col items-center">
                 <Link to='/' className="text-lg lg:text-xl xl:text-2xl font-bold"><span className="text-primary ">J</span>O<span className="text-primary ">B</span> <span className="font-light">PORTAL</span></Link>
-                    <p>© {new Date().getFullYear()} - JobPortal All right reserved. Design by <span className="text-blue-500">Jharna Khatun</span></p>
+                    <p className="text-gray-600 pl-15">© {new Date().getFullYear()} - JobPortal All right reserved. Design by <span className="text-blue-500">Jharna Khatun</span></p>
                 </aside>
                 <div>
-                    <p className="flex items-center gap-3 cursor-pointer">Back to top <BiSolidArrowToTop className="border text-2xl rounded-full p-1 bg-primary"/></p>
+                    <p className="flex items-center gap-3 cursor-pointer text-primary">Back to top <BiSolidArrowToTop className="border text-2xl rounded-full p-1 bg-primary"/></p>
                 </div>
             </div>
         </footer>
