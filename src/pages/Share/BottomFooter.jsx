2a33ed4239d7453c6cea6ca9ecda372d5a7edf33
Jharna203 <jharnakhatun203@gmail.com>
2025-03-28 22:27:24 +0600

    Responsive bottom footer

--- src/pages/Share/BottomFooter.jsx
+++ src/pages/Share/BottomFooter.jsx
@@ -5,14 +5,14 @@ import { BiSolidArrowToTop } from "react-icons/bi";
 const BottomFooter = () => {
     return (
         <footer className="bg-neutral text-neutral-content items-center p-4 ">
-            <div className="footer sm:footer-horizontal max-w-7xl mx-auto justify-between">
+            <div className="footer sm:footer-horizontal max-w-7xl mx-auto justify-items-center justify-between">
 
-                <aside className="grid-flow-col items-center">
+                <aside className="grid-flow-row sm:grid-flow-col items-center justify-items-center">
                 <Link to='/' className="text-lg lg:text-xl xl:text-2xl font-bold"><span className="text-primary ">J</span>O<span className="text-primary ">B</span> <span className="font-light">PORTAL</span></Link>
                     <p className="text-gray-600 pl-15">© {new Date().getFullYear()} - JobPortal All right reserved. Design by <span className="text-blue-500">Jharna Khatun</span></p>
                 </aside>
                 <div>
-                    <p className="flex items-center gap-3 cursor-pointer text-primary">Back to top <BiSolidArrowToTop className="border text-2xl rounded-full p-1 bg-primary"/></p>
+                    <p className="flex items-center gap-3 cursor-pointer text-primary">Back to top <BiSolidArrowToTop className="border border-primary text-2xl rounded-full p-1 bg-primary text-white"/></p>
                 </div>
             </div>
         </footer>
