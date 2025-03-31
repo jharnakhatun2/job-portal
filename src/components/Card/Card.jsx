ec971118a37a867de4e844a1e1921a5feb30d522
Jharna203 <jharnakhatun203@gmail.com>
2025-03-31 22:12:54 +0600

    Ctreated Job Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -20,9 +20,17 @@ const Card = () => {
     <div>
       {jobData.map((data)=>(
         <div key={data.id}>
-            <img src={data.logo} alt="company logo" />
+            <img src={data.logo} alt="company logo" className="border border-gray-200"/>
             <div>
-                
+               <h3 className="uppercase font-bold">{data.title}</h3> 
+               <p className="font-semibold">Posted <span className="ml-10">{data.posted}</span></p>
+               <p className="font-semibold">Specialism <span className="ml-10">{data.specialism}</span></p>
+               <p className="font-semibold">Location <span className="ml-10">{data.location}</span></p>
+               <p>{data.description}</p>
+            </div>
+            <div>
+                <p>{data.vacancy_type}</p>
+                <button>♡</button>
             </div>
         </div>
       ))}
