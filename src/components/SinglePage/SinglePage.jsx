963ba35c06226bb8278e63e8985502e2fc96da05
Jharna203 <jharnakhatun203@gmail.com>
2025-04-13 22:42:29 +0600

    Added Single page card info ui style

--- src/components/SinglePage/SinglePage.jsx
+++ src/components/SinglePage/SinglePage.jsx
@@ -5,8 +5,8 @@ const SinglePage = () => {
     <div className="sm:max-w-7xl mx-auto w-full px-3 xl:px-0 py-8">
       <div>
         <img src="/logo/logo1.png" alt="logo" className="border border-gray-200"/>
-        <div>
-        <h4 className="text-primary">Tix Dog</h4>
+        <div className="py-5 text-gray-500 text-sm space-y-1 tracking-wide">
+        <h4 className="text-primary text-lg font-light ">Tix Dog</h4>
         <div>
         <p>394 Edgware Road, London, W2 1ED</p>
         </div>
