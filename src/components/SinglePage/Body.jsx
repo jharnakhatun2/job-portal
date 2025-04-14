568c27aea602fab5e5c3a84d7513d2be5a961140
Jharna203 <jharnakhatun203@gmail.com>
2025-04-14 20:51:37 +0600

    Added skills required content

--- src/components/SinglePage/Body.jsx
+++ src/components/SinglePage/Body.jsx
@@ -2,14 +2,28 @@ import React from "react"
 
 const Body = () => {
   return (
-    <div>
+    <div className="text-gray-600 text-sm font-inter font-light">
       <div className="lg:flex gap-10">
         <div>
             <h2 className="uppercase font-semibold">Job Overview</h2>
             <div className="space-y-2 text-gray-600 text-sm font-inter font-light tracking-wide py-2">
-                <p className="">Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much dog out the much alas evasively neutral lazy reset.</p>
+                <p>Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much dog out the much alas evasively neutral lazy reset.</p>
                 <p>Repeatedly dreamed alas opossum but dramatically despite expeditiously that jeepers loosely yikes that as or eel underneath kept and slept compactly far purred sure abidingly up above fitting to strident wiped set waywardly far the and pangolin horse approving paid chuckled cassowary oh above a much opposite far much hypnotically more therefore wasp less that hey apart well like while superbly orca and far hence one.Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much dog out the much alas evasively neutral lazy reset.</p>
                 <p>Repeatedly dreamed alas opossum but dramatically despite expeditiously that jeepers loosely yikes that as or eel underneath kept and slept compactly far purred sure abidingly up above fitting to strident wiped set waywardly far the and.</p>
+            </div>
+            <h4 className="font-semibold text-[16px]">Skills Required</h4>
+            <div className="space-y-2 py-2">
+            <p>Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously. Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally.</p>
+            <div className="space-y-2">
+                <li>Strong Market connections for business development of the bank.</li>
+                <li>Strong knowledge of Corporate products and regulatory requirements.
+                </li>
+                <li>Proficiency in spoken and written English.
+                </li>
+                <li>Analytical skills
+                </li>
+                <li>Knowledge of Corporate Banking, Risk Management, Compliance and Central Bank regulations.</li>
+            </div>
             </div>
         </div>
         <div></div>
