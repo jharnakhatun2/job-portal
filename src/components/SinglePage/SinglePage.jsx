4520f090101eadf123f0d3692ce0513328310485
Jharna203 <jharnakhatun203@gmail.com>
2025-04-14 19:23:52 +0600

    Created Body and singleSidebar page for single page

--- src/components/SinglePage/SinglePage.jsx
+++ src/components/SinglePage/SinglePage.jsx
@@ -1,11 +1,18 @@
 import React from "react"
 import JobCard from "./JobCard";
+import Body from "./Body";
+import SingleSidebar from "./SingleSidebar";
 
 
 const SinglePage = () => {
   return (
     <div className="sm:max-w-7xl mx-auto w-full px-3 xl:px-0 py-8">
       <JobCard/>
+      <hr className="text-gray-200 my-2 sm:my-8 "/>
+      <div>
+        <Body/>
+        <SingleSidebar/>
+      </div>
     </div>
   )
 };
---
+++ src/components/SinglePage/SingleSidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SingleSidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default SingleSidebar;
