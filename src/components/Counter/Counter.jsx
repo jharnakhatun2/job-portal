fff364bcbf6d444b585ac58f46736b73877d1f01
Jharna203 <jharnakhatun203@gmail.com>
2025-04-20 22:19:08 +0600

    apply state from store in Counter component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,26 @@
 import React from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../../redux/Action/Action";
 
 const Counter = () => {
+    const count = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+    
+    const handleIncrement = () => {
+        dispatch(increment());
+    }
+    const handleDecrement = () => {
+        dispatch(decrement());
+    }
+    const handleReset = () =>{
+        dispatch(reset())
+    }
   return (
     <div>
-      <h1>Counter : 0</h1>
-      <button className="btn btn-primary">Increment</button>
-      <button className="btn btn-primary">Decrement</button>
+      <h1>Counter : {count}</h1>
+      <button onClick={handleIncrement} className="btn btn-primary">Increment</button>
+      <button onClick={handleDecrement} className="btn btn-primary">Decrement</button>
+      <button onClick={handleReset} className="btn btn-primary">Reset</button>
     </div>
   )
 };
