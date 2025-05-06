d90b6360556df4d5c15ab8d83ee9862d3e6aa634
Jharna203 <jharnakhatun203@gmail.com>
2025-05-06 21:23:28 +0600

    Created phone field in single page side bar

--- src/components/SinglePage/SingleSidebar.jsx
+++ src/components/SinglePage/SingleSidebar.jsx
@@ -4,7 +4,8 @@ const SingleSidebar = () => {
   return (
     <div className="text-gray-600 text-sm font-inter font-light min-w-xs">
       <h2 className="uppercase font-semibold pb-4">Contact</h2>
-      <form >    
+      <form >   
+        {/* Name field  */}
         <div className="relative mb-3">
           <input type="text" id="input-group-1" className="border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-3 p-2.5 outline-0" placeholder="Enter Your Name*" />
           <div className="absolute inset-y-0 end-3 flex items-center ps-3.5 pointer-events-none">
@@ -13,12 +14,22 @@ const SingleSidebar = () => {
             </svg>
           </div>
         </div>
-        <div class="relative mb-6">
-          <input type="text" id="input-group-1" className="border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-3 p-2.5 outline-0" placeholder="Email Address*" />
+        {/* Email Field */}
+        <div className="relative mb-6">
+          <input type="email" id="input-group-1" className="border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-3 p-2.5 outline-0" placeholder="Email Address*" />
           <div className="absolute inset-y-0 end-3 flex items-center ps-3.5 pointer-events-none">
             <svg className="w-3 h-3 text-gray-400 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
               <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
               <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </div>
+        </div>
+        {/* phone Field */}
+        <div className="relative mb-6">
+          <input type="text" id="input-group-1" className="border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-3 p-2.5 outline-0" placeholder="Phone Number*" />
+          <div className="absolute inset-y-0 end-3 flex items-center ps-3.5 pointer-events-none">
+          <svg className="w-4 h-4 text-gray-400 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fillRule="evenodd" clipRule="evenodd" d="M8 2C6.34315 2 5 3.34315 5 5V19C5 20.6569 6.34315 22 8 22H16C17.6569 22 19 20.6569 19 19V5C19 3.34315 17.6569 2 16 2H8ZM7 5C7 4.44772 7.44772 4 8 4H16C16.5523 4 17 4.44772 17 5V19C17 19.5523 16.5523 20 16 20H8C7.44772 20 7 19.5523 7 19V5ZM10 17C9.44772 17 9 17.4477 9 18C9 18.5523 9.44772 19 10 19H14C14.5523 19 15 18.5523 15 18C15 17.4477 14.5523 17 14 17H10Z" fill="currentColor"></path>
             </svg>
           </div>
         </div>
